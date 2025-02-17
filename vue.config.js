4851331887b9008839e234051a79af76e3623071
dapo4u <daporaphael@gmail.com>
2025-02-17 06:31:51 +0100

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/dapo4u.github.io/' : '/'
-  };
-  
+  publicPath: process.env.NODE_ENV === 'production' ? '/dapo4u.github.io/' : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/_variables.scss";`
+      }
+    }
+  }
+};
